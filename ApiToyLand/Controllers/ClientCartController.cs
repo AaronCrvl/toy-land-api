175f2f94014fc705955982f42701905bbef35a58
Aaron <72924198+AaronCrvl@users.noreply.github.com>
2023-07-07 16:54:22 -0300

    💳 adding client cart features

---
+++ ApiToyLand/Controllers/ClientCartController.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibraryToyLand.Data.Objects;
+using LibraryToyLand.Data.Lists;
+using ApiToyLand.Models;
+using Newtonsoft.Json;
+
+namespace ApiToyLand.Controllers
+{
+    public class ClientCartController : Controller
+    {
+        #region Endpoints
+        [HttpPost]
+        [EnableCors()]
+        [Route("StoreInCard/{idAccount}/{idProduct}")]
+        public ActionResult StoreInCart(int idAccount, int idProduct)
+        {
+            try
+            {
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Headers", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Methods", "*");
+
+                var success = ClientCart.AddProductToCart(idAccount, idProduct);
+                if (success)
+                {
+                    HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+                    var res200 = new ContentResult();
+                    res200.Content = "Product stored in cart.";
+                    res200.ContentType = "application/json";
+                    res200.ContentEncoding = System.Text.Encoding.UTF8;
+                    return res200;
+                }
+                else
+                {
+                    HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    var res400 = new ContentResult();
+                    res400.Content = "Something went wrong adding this product to cart.";
+                    res400.ContentType = "application/json";
+                    res400.ContentEncoding = System.Text.Encoding.UTF8;
+                    return res400;
+                }
+            }
+            catch (Exception ex)
+            {
+                return new System.Web.Mvc.HttpStatusCodeResult((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [EnableCors()]
+        [Route("GetCartCount/{idAccount}")]
+        public ActionResult GetCartCount(int idAccount)
+        {
+            try
+            {
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Headers", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Methods", "*");
+
+                int count = ClientCart.GetCartCount(idAccount);
+
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+                var res200 = new ContentResult();
+                res200.Content = count.ToString();
+                res200.ContentType = "application/json";
+                res200.ContentEncoding = System.Text.Encoding.UTF8;
+                return res200;
+            }
+            catch (Exception ex)
+            {
+                return new System.Web.Mvc.HttpStatusCodeResult((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [EnableCors()]
+        [Route("RemoveProductFromCart/{idAccount}/{idProduct}")]
+        public ActionResult RemoveProductFromCart(int idAccount, int idProduct)
+        {
+            try
+            {
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Headers", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Methods", "*");
+
+                bool removeSucceded = ClientCart.RemoveProductFromCart(idAccount, idProduct);
+                if (removeSucceded)
+                {
+                    HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+                    var res200 = new ContentResult();
+                    res200.Content = "Product removed from cart";
+                    res200.ContentType = "application/json";
+                    res200.ContentEncoding = System.Text.Encoding.UTF8;
+                    return res200;
+                }
+                else
+                {
+                    HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    var res400 = new ContentResult();
+                    res400.Content = "Failed removing product from cart";
+                    res400.ContentType = "application/json";
+                    res400.ContentEncoding = System.Text.Encoding.UTF8;
+                    return res400;
+                }
+            }
+            catch (Exception ex)
+            {
+                return new System.Web.Mvc.HttpStatusCodeResult((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [EnableCors()]
+        [Route("GetCartCount/{idAccount}")]
+        public ActionResult GetCartProducts(int idAccount)
+        {
+            try
+            {
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Headers", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "*");
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Methods", "*");
+
+                var list = new ListClientCart().LoadProductList(idAccount);
+                var finalList = FillProductModelList(list).AsEnumerable().OrderBy(x => x.idProduct);
+
+                if (list.Count == 0)
+                    return new HttpStatusCodeResult((int)HttpStatusCode.NotFound);
+                else
+                {
+                    var DataResult = new ContentResult();
+                    var jsonString = JsonConvert.SerializeObject(finalList);
+                    DataResult.ContentType = "application/json";
+                    DataResult.ContentEncoding = System.Text.Encoding.UTF8;
+                    DataResult.Content = jsonString;
+                    return DataResult;
+                }
+            }
+            catch (Exception ex)
+            {
+                return new System.Web.Mvc.HttpStatusCodeResult((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        #endregion
+
+        #region Methods
+        private List<ProductModel> FillProductModelList(List<Product> pList)
+        {
+            var list = new List<ProductModel>();
+            foreach (Product p in pList)
+            {
+                var model = new ProductModel();
+                model.idProduct = p.IdProduct.ToString();
+                model.productName = p.ProductName;
+                model.shortDescription = p.ShortDescription;
+                model.imageUrl = p.ImageUrl;
+                list.Add(model);
+            }
+            return list;
+        }
+        #endregion
+    }
+}
---
+++ LibraryToyLand/Data/Objects/ClientCart.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryToyLand.Data.Objects
+{
+    public class ClientCart
+    {
+        #region Constructors
+        public ClientCart()
+        {
+            this.idAccount = -1;
+            this.idProduct = -1;
+        }
+
+        public ClientCart(int idAcct)
+        {
+            this.idAccount = idAcct;
+            this.idProduct = -1;
+        }
+
+        public ClientCart(int idAcct, int idProduct)
+        {
+            this.idAccount = idAcct;
+            this.idProduct = idProduct;
+        }
+        #endregion        
+
+        #region Variables
+        public int idAccount;
+        public int idProduct;
+        #endregion
+
+        #region Methods        
+        public bool Validate() 
+        {
+            if (idAccount > 0 && idProduct > 0)
+            {
+                StringBuilder query = new StringBuilder();
+                query.AppendLine(" SELECT ");
+                query.AppendLine(" ID_ACCT, ");
+                query.AppendLine(" ID_PRODUCT, ");
+                query.AppendLine(" FROM [dbo].[ClientCart](NOLOCK) ");
+                query.AppendLine(" WHERE ID_ACCT = @ID_ACCT AND ID_PRODUCT = @ID_PRODUCT ");
+
+                Framework.Database.Transaction.ExecuteSelectSingleObjectCommand(query.ToString(), new string[]
+                {
+                    "@ID_ACCT", idAccount.ToString(),
+                    "@ID_PRODUCT", idProduct.ToString()
+                });
+            }
+
+            return false;
+        }
+
+        public static int GetCartCount(int idAcct)
+        {
+            StringBuilder query = new StringBuilder();
+            query.AppendLine(" SELECT COUNT(*) AS QTD ");                        
+            query.AppendLine(" FROM [dbo].[ClientCart](NOLOCK) ");
+            query.AppendLine(" WHERE ID_ACCT = @ID_ACCT ");
+
+            var obj = Framework.Database.Transaction.ExecuteSelectSingleObjectCommand(query.ToString(), 
+                new string[] { "@ID_ACCT", idAcct.ToString() });
+
+            if (obj == null)
+                return 0;
+            else 
+            {
+                int count = obj.Field<int>("QTD");
+                return count;
+            }
+        }
+
+        public static bool AddProductToCart(int idAcct, int idProduct)
+        {
+            bool execute = false;
+            if (idAcct > 0 && idProduct > 0)
+            {
+                StringBuilder _query = new StringBuilder();
+                _query.AppendLine(" SELECT ");
+                _query.AppendLine(" ID_ACCT, ");
+                _query.AppendLine(" ID_PRODUCT ");
+                _query.AppendLine(" FROM [dbo].[ClientCart](NOLOCK) ");
+                _query.AppendLine(" WHERE ID_ACCT = @ID_ACCT AND ID_PRODUCT = @ID_PRODUCT ");
+
+                var obj = Framework.Database.Transaction.ExecuteSelectSingleObjectCommand(_query.ToString(), new string[]
+                {
+                    "@ID_ACCT", idAcct.ToString(),
+                    "@ID_PRODUCT", idProduct.ToString()
+                });
+
+                if (obj == null)
+                    execute = true;
+
+                if (execute)
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine(" INSERT INTO [dbo].[ClientCart] (ID_ACCT, ID_PRODUCT) ");
+                    query.AppendLine(" VALUES (@ID_ACCT, @ID_PRODUCT) ");
+
+                    Framework.Database.Transaction.ExecuteCreateObjectCommand(query.ToString(), new string[]
+                    {
+                        "@ID_ACCT", idAcct.ToString(),
+                        "@ID_PRODUCT", idProduct.ToString()
+                    });
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool RemoveProductFromCart(int idAcct, int idProduct)
+        {
+            bool execute = false;
+            if (idAcct > 0 && idProduct > 0)
+            {
+                StringBuilder _query = new StringBuilder();
+                _query.AppendLine(" SELECT ");
+                _query.AppendLine(" ID_ACCT, ");
+                _query.AppendLine(" ID_PRODUCT ");
+                _query.AppendLine(" FROM [dbo].[ClientCart](NOLOCK) ");
+                _query.AppendLine(" WHERE ID_ACCT = @ID_ACCT AND ID_PRODUCT = @ID_PRODUCT ");
+
+                var obj = Framework.Database.Transaction.ExecuteSelectSingleObjectCommand(_query.ToString(), new string[]
+                {
+                    "@ID_ACCT", idAcct.ToString(),
+                    "@ID_PRODUCT", idProduct.ToString()
+                });
+
+                if (obj != null)
+                    execute = true;
+
+                if (execute)
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine(" DELETE FROM ClientCart ");
+                    query.AppendLine(" WHERE ID_ACCT = @ID_ACCT AND ID_PRODUCT = @ID_PRODUCT ");
+
+                    Framework.Database.Transaction.ExecuteDeleteSingleObjectCommand(query.ToString(), new string[]
+                    {
+                        "@ID_ACCT", idAcct.ToString(),
+                        "@ID_PRODUCT", idProduct.ToString()
+                    });
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+        #endregion
+    }
+}
